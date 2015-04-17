b10f830ea97760d93c612c89877f709cd169f841
Gen2 <gen2@kek.club>
2015-04-17 23:51:25 +0100

    Restore old ";" replacing functionality

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 function kek() {
-	spaghetti = document.getElementById("box").value;
-	spaghetti = spaghetti.replace(/	/g, "");
-	spaghetti = spaghetti.replace(/;/g, ";");
-
-	document.getElementById("box").value = spaghetti;
+	document.getElementById("box").value = document.getElementById("box").value.replace(/;/g, ";");
 }
