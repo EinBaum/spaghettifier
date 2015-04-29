921466e05dd916554755cdba860ddfcfc7b0023a
Gen2 <gen2@kek.club>
2015-04-29 16:20:33 +0100

    Add "o" replacement (thanks mangofett from IRC)

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 function kek() {
 	document.getElementById("box").value = document.getElementById("box").value.replace(/;/g, ";");
+	document.getElementById("box").value = document.getElementById("box").value.replace(/o/g, "о");
 }
